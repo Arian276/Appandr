fd0068c2b5b6b8061b8cd3a65b7329b20850c316
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-20 20:55:59 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
+// Archivo de nivel proyecto (no el del módulo app)
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
 }
