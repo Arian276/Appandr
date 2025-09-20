d0deaa5a5439ba687acbb14a860babcaa64c7114
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-20 04:09:08 -0300

    Update MainActivity.kt

--- app/src/main/java/com/bc/tvappvlc/MainActivity.kt
+++ app/src/main/java/com/bc/tvappvlc/MainActivity.kt
@@ -15,19 +15,19 @@ class MainActivity : AppCompatActivity() {
 
         val rv = findViewById<RecyclerView>(R.id.recyclerView)
         if (rv == null) {
-            Toast.makeText(this, "No se encontró RecyclerView en activity_main", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "No se encontró recyclerView en activity_main", Toast.LENGTH_LONG).show()
             return
         }
 
         rv.layoutManager = GridLayoutManager(this, 2)
 
         val channels = listOf(
-            Channel(id = "espnp", name = "ESPN Premium",     category = "Deportes", url = "url1"),
-            Channel(id = "tnts",  name = "TNT Sports",       category = "Deportes", url = "url2"),
-            Channel(id = "dsv",   name = "DirecTV Sports",   category = "Deportes", url = "url3"),
-            Channel(id = "dtv+",  name = "DirecTV+",         category = "Entretenimiento", url = "url4"),
-            Channel(id = "espn",  name = "ESPN HD",          category = "Deportes", url = "url5"),
-            Channel(id = "espn2", name = "ESPN 2 HD",        category = "Deportes", url = "url6")
+            Channel(id = "espnp", name = "ESPN Premium",   category = "Deportes", url = "url1"),
+            Channel(id = "tnts",  name = "TNT Sports",     category = "Deportes", url = "url2"),
+            Channel(id = "dsv",   name = "DirecTV Sports", category = "Deportes", url = "url3"),
+            Channel(id = "dtv+",  name = "DirecTV+",       category = "Entretenimiento", url = "url4"),
+            Channel(id = "espn",  name = "ESPN HD",        category = "Deportes", url = "url5"),
+            Channel(id = "espn2", name = "ESPN 2 HD",      category = "Deportes", url = "url6")
         )
 
         rv.adapter = ChannelAdapter(channels)
