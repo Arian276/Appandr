122dc47d1a53914a20e463821a53cd906b056993
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-20 03:24:21 -0300

    Update Mappers.kt

--- app/src/main/java/com/bc/tvappvlc/model/Mappers.kt
+++ app/src/main/java/com/bc/tvappvlc/model/Mappers.kt
@@ -1,13 +1,8 @@
-package com.bc.tvappvlc.model
-
-// Función de extensión que transforma ChannelDto → Channel (UI)
-fun ChannelDto.toUiModel(): Channel {
-    return Channel(
-        name = this.name,
-        logo = this.logo,
-        url = this.stream_url,
-        category = this.category,
-        resolution = this.quality,
-        viewer_count = this.viewers
-    )
-}
+fun fromJson(dto: ChannelDto): Channel = Channel(
+    id = dto.id,
+    name = dto.name,
+    url = dto.url,
+    logo = dto.logo,
+    resolution = dto.resolution,
+    viewer_count = dto.viewer_count
+)
