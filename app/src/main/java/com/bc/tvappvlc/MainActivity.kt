06fbee0c8520b5f2c07c87f161fbba104f74828f
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-20 20:22:43 -0300

    Update MainActivity.kt

--- app/src/main/java/com/bc/tvappvlc/MainActivity.kt
+++ app/src/main/java/com/bc/tvappvlc/MainActivity.kt
@@ -1,16 +1,20 @@
-package com.bc.tvappvlc.ui
+package com.bc.tvappvlc
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import com.bc.tvappvlc.PlayerActivity
+import com.bc.tvappvlc.ui.HomeScreen
+import com.bc.tvappvlc.ui.initThemeTokensIfNeeded
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // ⬇️ Carga tokens.json y animations.json sólo una vez
+        initThemeTokensIfNeeded(this)
 
         setContent {
             HomeScreen { url, title ->
