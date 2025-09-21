f7670fa90e5584c72fd1ac44cf781158a09cad63
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-21 19:45:34 -0300

    Delete gradlew.bat

--- gradlew.bat
+++
@@ -1,8 +0,0 @@
-\
-@ECHO OFF
-set CLASSPATH=%~dp0\gradle\wrapper\gradle-wrapper.jar
-IF NOT "%JAVA_HOME%"=="" (
-  "%JAVA_HOME%\bin\java.exe" -Xmx64m -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-) ELSE (
-  java -Xmx64m -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
-)
