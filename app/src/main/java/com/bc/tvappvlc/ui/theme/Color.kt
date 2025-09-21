76b9ba9d0337f16d4013959346f6e107480df1bb
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-21 01:13:54 -0300

    Create Color.kt

---
+++ app/src/main/java/com/bc/tvappvlc/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.bc.tvappvlc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TealGreen = Color(0xFF00C3AD)        // color para botones "Ver Ahora"
+val SurfaceDark = Color(0xFF1E1E1E)      // fondo principal oscuro
+val SurfaceVariantDark = Color(0xFF2A2A2A) // tarjetas ligeramente más claras
+val OnSurfaceDark = Color(0xFFF5F5F5)
+val OnSurfaceVariantDark = Color(0xFFAAAAAA)
