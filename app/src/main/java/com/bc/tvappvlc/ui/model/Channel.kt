334aa2177dd0224e9b2d131a7df3e3bc213b0ff2
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-21 01:11:12 -0300

    Update Channel.kt

--- app/src/main/java/com/bc/tvappvlc/ui/model/Channel.kt
+++ app/src/main/java/com/bc/tvappvlc/ui/model/Channel.kt
@@ -3,9 +3,9 @@ package com.bc.tvappvlc.model
 data class Channel(
     val name: String,
     val logo: String,
-    val url: String,
-    val description: String,
     val quality: String,
-    val category: String,
-    val viewers: Int
+    val viewers: Int,
+    val description: String,
+    val cta: String,
+    val url: String
 )
