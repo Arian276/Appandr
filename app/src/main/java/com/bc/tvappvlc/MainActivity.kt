ecb95766fde7a85672a3b9a845aa9d820c9de6c3
Arian276 <114463200+Arian276@users.noreply.github.com>
2025-09-20 22:01:48 -0300

    Update MainActivity.kt

--- app/src/main/java/com/bc/tvappvlc/MainActivity.kt
+++ app/src/main/java/com/bc/tvappvlc/MainActivity.kt
@@ -3,14 +3,28 @@ package com.bc.tvappvlc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.bc.tvappvlc.model.Channel
 import com.bc.tvappvlc.ui.HomeScreen
+import com.bc.tvappvlc.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            HomeScreen()
+            AppTheme {
+                // TODO: reemplazar con datos cargados de JSON remoto
+                val sampleChannels = listOf(
+                    Channel(id = "1", name = "Canal Demo 1", streamUrl = "http://test/1"),
+                    Channel(id = "2", name = "Canal Demo 2", streamUrl = "http://test/2")
+                )
+
+                HomeScreen(
+                    channels = sampleChannels,
+                    onChannelClick = { channel ->
+                        // TODO: lanzar PlayerActivity con channel.streamUrl
+                    }
+                )
+            }
         }
     }
 }
